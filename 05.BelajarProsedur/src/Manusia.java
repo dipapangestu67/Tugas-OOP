a8990a32c15c9ea4cc4e2f7da07ddc52e3ca03d8
dipapangestu67 <113498837+dipapangestu67@users.noreply.github.com>
2022-10-31 16:40:04 +0700

    Add files via upload

---
+++ 05.BelajarProsedur/src/Manusia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class Manusia {
+    String nama;
+    String jenisKelamin;
+    String alamat;
+    
+    void tampilInformasi(){
+        System.out.println("Nama : " + nama);
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        System.out.println("Alamat : " + alamat);
+}
+}
